bd54831ddc4feaea0bed8d3f3742d6c5c2f77e51
Pedro Rezende <pedro_rezende09@yahoo.com.br>
2025-12-15 21:41:19 -0300

    feat: add ProdutoVidro resource controller and routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProdutoVidroController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('produtos', ProdutoVidroController::class);
